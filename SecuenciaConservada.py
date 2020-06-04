8f5f3dc4848e99951e98001dc0331759dcd568c3
Jesus Alberto Correa Morales <jesus_chivas_5000@hotmail.com>
2020-06-04 12:01:46 -0500

    Se optimizo codigo.

--- SecuenciaConservada.py
+++ SecuenciaConservada.py
@@ -5,32 +5,39 @@ import os
 class Secuencia(object):
     ind = {'A':0, 'C':1, 'D':2, 'E':3, 'F':4, 'G':5, 'H':6, 'I':7, 'K':8, 'L':9, 'M':10, 'N':11, 'P':12, 'Q':13, 'R':14, 'S':15, 'T':16, 'V':17, 'W':18, 'Y':19, '-':20}
     hongos = HG().matrizHongos()
+    numHongos = len(hongos)
+    tamHongo = len(hongos[0])
     
     def __init__(self, numSecuenciasConservadas = 50, tamSec = 30):
         self.numSecuenciasConservadas = numSecuenciasConservadas 
-        self.tamSec = 30
+        self.tamSec = tamSec
         self.ConSecCon = []
+
+        # Inicializamos el contador.
+        self.contador = [[0 for _ in range(len(self.ind))] for _ in range(self.tamHongo)]   
+
+        # contador [i][j] 
+        # i -> representa la columna de la secuencia
+        # j -> representa la columna de los aminoacidos
+
+        for i in range(self.tamHongo):
+            for j in range(self.numHongos):
+                self.contador[i][self.ind[self.hongos[j][i]]] = self.contador[i][self.ind[self.hongos[j][i]]] + 1
+                pass
+            pass
+
         for _ in range(self.numSecuenciasConservadas):
             ps = random.randrange(len(self.hongos[0])-self.tamSec)
             sec = []
             sumSecCon = 0 
 
-            # Inicializamos el contador.
-            contador = [[0 for _ in range(len(self.ind))] for _ in range(self.tamSec)]
-
             for i in range(self.tamSec):
-                for j in range(len(self.hongos)):
-                    contador[i][self.ind[self.hongos[j][i + ps]]] = contador[i][self.ind[self.hongos[j][i + ps]]] +1
-                    pass
-                pass
-
-            for am in contador:
-                index = am.index(max(am))
+                index = self.contador[ps + i].index(max(self.contador[ps + i]))
                 if index == 20:
-                    am.pop(index)
-                    index = am.index(max(am))
+                    self.contador[ps + i].pop(index)
+                    index = self.contador[ps + i].index(max(self.contador[ps + i]))
                     pass
-                sumSecCon = sumSecCon + max(am)
+                sumSecCon = sumSecCon + max(self.contador[ps + i])
                 sec.append(list(self.ind.keys())[index])
                 pass
             self.ConSecCon.append([sec, 100 * float(sumSecCon)/(self.tamSec * len(self.hongos)), ps])
@@ -40,29 +47,34 @@ class Secuencia(object):
 
     def ConjuntoSecuenciaConservada(self,numSecuenciasConservadas = 50, tamSec = 30):
         self.numSecuenciasConservadas = numSecuenciasConservadas 
-        self.tamSec = 30
+        self.tamSec = tamSec
         self.ConSecCon = []
+
+        # Inicializamos el contador.
+        self.contador = [[0 for _ in range(len(self.ind))] for _ in range(self.tamHongo)]   
+
+        # contador [i][j] 
+        # i -> representa la columna de la secuencia
+        # j -> representa la columna de los aminoacidos
+
+        for i in range(self.tamHongo):
+            for j in range(self.numHongos):
+                self.contador[i][self.ind[self.hongos[j][i]]] = self.contador[i][self.ind[self.hongos[j][i]]] + 1
+                pass
+            pass
+
         for _ in range(self.numSecuenciasConservadas):
             ps = random.randrange(len(self.hongos[0])-self.tamSec)
             sec = []
             sumSecCon = 0 
 
-            # Inicializamos el contador.
-            contador = [[0 for _ in range(len(self.ind))] for _ in range(self.tamSec)]
-
             for i in range(self.tamSec):
-                for j in range(len(self.hongos)):
-                    contador[i][self.ind[self.hongos[j][i + ps]]] = contador[i][self.ind[self.hongos[j][i + ps]]] +1
-                    pass
-                pass
-
-            for am in contador:
-                index = am.index(max(am))
+                index = self.contador[ps + i].index(max(self.contador[ps + i]))
                 if index == 20:
-                    am.pop(index)
-                    index = am.index(max(am))
+                    self.contador[ps + i].pop(index)
+                    index = self.contador[ps + i].index(max(self.contador[ps + i]))
                     pass
-                sumSecCon = sumSecCon + max(am)
+                sumSecCon = sumSecCon + max(self.contador[ps + i])
                 sec.append(list(self.ind.keys())[index])
                 pass
             self.ConSecCon.append([sec, 100 * float(sumSecCon)/(self.tamSec * len(self.hongos)), ps])
@@ -75,7 +87,21 @@ if __name__ == "__main__":
     ind = {'A':0, 'C':1, 'D':2, 'E':3, 'F':4, 'G':5, 'H':6, 'I':7, 'K':8, 'L':9, 'M':10, 'N':11, 'P':12, 'Q':13, 'R':14, 'S':15, 'T':16, 'V':17, 'W':18, 'Y':19, '-':20}
     hongos = HG().matrizHongos()
     tamSec = 2
-    ruta = "SecuenciaConservada"
+    ruta = "SecuenciaConservada455"
+    numHongos = len(hongos)
+    tamHongo = len(hongos[0])
+    # Inicializamos el contador.
+    contador = [[0 for _ in range(len(ind))] for _ in range(tamHongo)]   
+
+    for i in range(tamHongo):
+        for j in range(numHongos):
+            contador[i][ind[hongos[j][i]]] = contador[i][ind[hongos[j][i]]] + 1
+            pass
+        pass
+
+    # contador [i][j] 
+    # i -> representa la columna de la secuencia
+    # j -> representa la columna de los aminoacidos
 
     if not os.path.exists(ruta):
         os.makedirs(ruta)
@@ -90,26 +116,17 @@ if __name__ == "__main__":
             file.write(os.linesep)
             ConSecCon = []
             top = 0.0
-            for ps in range(len(hongos[0])-tamSec):
+
+            for ps in range(len(hongos[0]) - tamSec):
+                sumSecCon = 0
                 sec = []
-                sumSecCon = 0 
-
-                # Inicializamos el contador.
-                contador = [[0 for _ in range(len(ind))] for _ in range(tamSec)]
-
                 for i in range(tamSec):
-                    for j in range(len(hongos)):
-                        contador[i][ind[hongos[j][i + ps]]] = contador[i][ind[hongos[j][i + ps]]] +1
-                        pass
-                    pass
-
-                for am in contador:
-                    index = am.index(max(am))
+                    index = contador[ps + i].index(max(contador[ps + i]))
                     if index == 20:
-                        am.pop(index)
-                        index = am.index(max(am))
+                        contador[ps + i].pop(index)
+                        index = contador[ps + i].index(max(contador[ps + i]))
                         pass
-                    sumSecCon = sumSecCon + max(am)
+                    sumSecCon = sumSecCon + max(contador[ps + i])
                     sec.append(list(ind.keys())[index])
                     pass
                 ConSecCon.append([sec, 100 * float(sumSecCon)/(tamSec * len(hongos))])
