edab32f66d5df421883db9723a4d2449f68421fa
Jesus Alberto Correa Morales <jesus_chivas_5000@hotmail.com>
2020-09-17 20:14:06 -0500

    Se cambio de nombre al archivo.

--- Similitud.py
+++ Similitud.py
@@ -1,7 +1,7 @@
 from Hongos import Hongos as HG 
 import os 
 
-class Posicion(object):
+class Similitud(object):
     hongos = HG().matrizHongos()
     ind = {'A':0, 'C':1, 'D':2, 'E':3, 'F':4, 'G':5, 'H':6, 'I':7, 'K':8, 'L':9, 'M':10, 'N':11, 'P':12, 'Q':13, 'R':14, 'S':15, 'T':16, 'V':17, 'W':18, 'Y':19, '-':20}
     
