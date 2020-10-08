f110d148ec1067a673628d272b9c15c440d6e22a
Jesus Alberto Correa Morales <jesus_chivas_5000@hotmail.com>
2020-10-08 07:53:54 -0500

    Se creo plantilla para la busqueda de similitud.

--- Similitud.py
+++ Similitud.py
@@ -72,50 +72,10 @@ if __name__ == "__main__":
     hongos = HG().matriz_hongos()
     nombres = HG().lista_nombres()
     secs_referencia = [
-        ['G','S','G','N','F','S','A','I','D','Q'],
-        ['L','T','G','M','K','V','A','H','C','D'],
-        ['C','E','R','V','G','V','A','Q','Y','D'],
-        ['G','P','S','G','H','A','M','P','Q','D'],
-        ['T','R','H','F','P','M','Y','G','V','D'],
-        ['A','S','Q','K','L','V','G','A','M','F'],
-        ['A','W','N','K','G','S','S','I','L','D'],
-        ['Y','D','I','V','G','P','N','I','Y','D'],
-        ['R','N','S','V','G','V','A','D','V','E'],
-        ['R','S','G','V','Q','G','G','I','W','H'],
-        ['I','T','L','Y','T','H','W','F','P','K'],
-        ['H','D','Y','C','A','P','F','R','T','P'],
-        ['V','T','N','I','C','C','F','Y','Y','R'],
-        ['Y','Y','P','I','S','L','K','L','A','I'],
-        ['A','L','F','L','L','E','L','Y','E','D'],
-        ['P','G','G','V','H','K','S','C','P','P'],
-        ['L','M','S','V','F','L','M','N','I','W'],
-        ['T','K','V','D','L','Y','F','V','M','P'],
-        ['R','M','S','A','H','R','P','T','S','Q'],
-        ['Q','D','R','A','S','D','V','V','D','D'],
-        ['W','M','C','P','Y','A','V','N','L','T'],
-        ['T','V','P','Q','H','I','K','L','V','P'],
-        ['G','S','P','Y','I','I','P','A','L','F'],
-        ['S','M','K','E','Y','I','M','T','Y','M'],
-        ['K','Y','V','P','C','K','M','M','V','W'],
-        ['Q','Y','P','W','D','A','R','W','L','P'],
-        ['F','T','L','N','C','K','T','H','W','V'],
-        ['G','A','Y','S','H','E','D','Y','H','F'],
-        ['V','M','A','I','D','V','V','N','L','S'],
-        ['A','A','C','V','M','C','T','A','T','D'],
-        ['E','N','P','M','T','M','A','L','E','N'],
-        ['V','H','V','S','M','E','P','K','N','L'],
-        ['G','Y','F','S','R','D','F','C','G','V'],
-        ['R','A','L','R','E','D','M','V','P','P'],
-        ['F','R','M','H','P','L','L','T','I','G'],
-        ['F','T','V','P','I','V','V','P','M','L'],
-        ['K','W','V','G','L','D','R','C','K','W'],
-        ['D','S','W','V','M','P','F','Q','C','H'],
-        ['N','I','M','V','H','R','L','V','G','F'],
-        ['N','V','H','V','H','N','I','P','F','F']
     ]
 
     ind = {'A':0, 'C':1, 'D':2, 'E':3, 'F':4, 'G':5, 'H':6, 'I':7, 'K':8, 'L':9, 'M':10, 'N':11, 'P':12, 'Q':13, 'R':14, 'S':15, 'T':16, 'V':17, 'W':18, 'Y':19, '-':20}
-    ruta = "SimilitudSSAenAMS"
+    ruta = "SimilitudSSAenAMS/Comp/ruta"
     num_hongos = len(hongos)
     tam_hongo = len(hongos[0])
 
@@ -133,6 +93,8 @@ if __name__ == "__main__":
     # contador [i][j] 
     # i -> representa la columna de la secuencia
     # j -> representa la columna de los aminoacidos
+    
+    file2 = open(ruta + "/SimilitudConcentrada.txt", "w")
 
     for sec_ref in secs_referencia:
         
@@ -161,6 +123,7 @@ if __name__ == "__main__":
         file.write(os.linesep)
         file.write("La similitud maxima se encontro entre las columnas " + str(c + 1) + " y " + str(c + tam_sec) + os.linesep)
         file.write("Submatriz:" + os.linesep + os.linesep)
+        file2.write(str(conttxt) + ";" + str(sec_ref) + ";     " + str(maxhomologo) + ";" + str(c + 2) + os.linesep)
         for i in range(num_hongos):
             # file.write(str(nombres[i]) + os.linesep)
             file.write(str(hongos[i][c:c + tam_sec]) + os.linesep)
